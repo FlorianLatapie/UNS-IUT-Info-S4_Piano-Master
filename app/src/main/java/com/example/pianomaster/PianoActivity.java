f82474cd767dd83d6d8fc656d246fe076d1b5642
pierrebihannic <pierre.bihannic@etu.unice.fr>
2021-03-13 15:30:47 +0100

    Erreur corrigé

--- app/src/main/java/com/example/pianomaster/PianoActivity.java
+++ app/src/main/java/com/example/pianomaster/PianoActivity.java
@@ -4,9 +4,4 @@ import android.app.Activity;
 
 public class PianoActivity extends Activity {
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.piano_activity);
-    }
 }
--- app/src/main/java/com/example/pianomaster/QuestionActivity.java
+++ app/src/main/java/com/example/pianomaster/QuestionActivity.java
@@ -4,9 +4,4 @@ import android.app.Activity;
 
 public class QuestionActivity extends Activity {
 
-
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.question_activity);
-    }}
+}

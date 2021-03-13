7bb2a94b05788e0ffbee499a7cee101ec4abd453
pierrebihannic <pierre.bihannic@etu.unice.fr>
2021-03-13 16:08:48 +0100

    Ajout bouton listener

--- app/src/main/java/com/example/pianomaster/LevelActivity.java
+++ app/src/main/java/com/example/pianomaster/LevelActivity.java
@@ -1,9 +1,31 @@
 package com.example.pianomaster;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import java.util.List;
 
 
 public class LevelActivity extends Activity {
+    private List<Button> listButtonLevel;
 
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_choix_niveau);
+        initButtonsLevel();
+
+    }
+
+    public void initButtonsLevel(){
+        listButtonLevel.add(findViewById(R.id.b_niv_1));
+        listButtonLevel.add(findViewById(R.id.b_niv_2));
+        listButtonLevel.add(findViewById(R.id.b_niv_3));
+        listButtonLevel.add(findViewById(R.id.b_niv_4));
+        listButtonLevel.add(findViewById(R.id.b_niv_5));
+        listButtonLevel.add(findViewById(R.id.b_niv_6));
+    }
 }
--- app/src/main/java/com/example/pianomaster/MainActivity.java
+++ app/src/main/java/com/example/pianomaster/MainActivity.java
@@ -2,13 +2,24 @@ package com.example.pianomaster;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+    private Button playButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        playButton = findViewById(R.id.b_jouer);
+        playButton.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, LevelActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }

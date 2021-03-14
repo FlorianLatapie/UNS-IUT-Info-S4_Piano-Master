dd2bafe39b0e1192d43d3923a4d5c9b0d93ea418
pierrebihannic <pierre.bihannic@etu.unice.fr>
2021-03-14 11:52:20 +0100

    Mis a jour Question

--- app/src/main/java/com/example/pianomaster/LevelActivity.java
+++ app/src/main/java/com/example/pianomaster/LevelActivity.java
@@ -1,31 +1,28 @@
 package com.example.pianomaster;
 
 import android.app.Activity;
+import android.app.ProgressDialog;
 import android.content.Intent;
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
 import java.util.List;
 
 
 public class LevelActivity extends Activity {
-    private List<Button> listButtonLevel;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_choix_niveau);
-        initButtonsLevel();
-
-    }
-
-    public void initButtonsLevel(){
-        listButtonLevel.add(findViewById(R.id.b_niv_1));
-        listButtonLevel.add(findViewById(R.id.b_niv_2));
-        listButtonLevel.add(findViewById(R.id.b_niv_3));
-        listButtonLevel.add(findViewById(R.id.b_niv_4));
-        listButtonLevel.add(findViewById(R.id.b_niv_5));
-        listButtonLevel.add(findViewById(R.id.b_niv_6));
     }
 }
--- app/src/main/java/com/example/pianomaster/Question.java
+++ app/src/main/java/com/example/pianomaster/Question.java
@@ -1,11 +1,21 @@
 package com.example.pianomaster;
 
+import android.app.ProgressDialog;
+import android.os.AsyncTask;
+import android.util.Log;
+import android.widget.ArrayAdapter;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
 import java.util.List;
 
 public class Question {
     private String titre;
 
-    public Question(String titre){
+    public Question(String titre) {
         this.titre = titre;
     }
 

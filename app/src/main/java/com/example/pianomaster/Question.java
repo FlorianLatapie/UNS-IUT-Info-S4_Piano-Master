bb69a6a229371b107a8de2bc2c8b9efb6f3d2dee
Florian LATAPIE <70631622+FlorianLatapie@users.noreply.github.com>
2021-03-19 00:23:45 +0100

    ajout du numéro de question

--- app/src/main/java/com/example/pianomaster/Question.java
+++ app/src/main/java/com/example/pianomaster/Question.java
@@ -13,17 +13,29 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Question {
+    private String numQuestion;
     private String titre;
 
     public Question(String titre) {
         this.titre = titre;
+        this.numQuestion = "num question non defini";
+    }
+    public Question(String numQuestion, String titre) {
+        this.titre = titre;
+        this.numQuestion = numQuestion;
     }
 
     public String getTitre() {
         return titre;
     }
+    public String getNumQuestion() {
+        return numQuestion;
+    }
 
     public void setTitre(String titre) {
         this.titre = titre;
+    }
+    public void setNumQuestion(String numQuestion) {
+        this.numQuestion = numQuestion;
     }
 }

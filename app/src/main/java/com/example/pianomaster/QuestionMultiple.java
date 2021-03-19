4756fa4b87c7dd1a3f7ed104c9cb2e46a2b222d6
pierrebihannic <pierre.bihannic@etu.unice.fr>
2021-03-19 15:46:48 +0100

    ptit push (oupsi)

--- app/src/main/java/com/example/pianomaster/QuestionMultiple.java
+++ app/src/main/java/com/example/pianomaster/QuestionMultiple.java
@@ -1,17 +1,50 @@
 package com.example.pianomaster;
 
+import android.os.Parcel;
+import android.os.Parcelable;
+
 import java.util.List;
 
-public class QuestionMultiple extends Question {
+public class QuestionMultiple extends Question implements Parcelable {
     private List<String> reponses;
     private String response;
     private String idImage;
+    private String url;
 
-    public QuestionMultiple(String numQuestion, String titre, String idImage, List<String> reponses, String response){
+    public QuestionMultiple(String numQuestion, String titre, String idImage, String url, List<String> reponses, String response){
         super(numQuestion,titre);
         this.reponses = reponses;
         this.response = response;
         this.idImage = idImage;
+        this.url = url;
+    }
+
+    protected QuestionMultiple(Parcel in) {
+        super(in.readString(), in.readString());
+        idImage = in.readString();
+        url = in.readString();
+        reponses = in.createStringArrayList();
+        response = in.readString();
+    }
+
+    public static final Creator<QuestionMultiple> CREATOR = new Creator<QuestionMultiple>() {
+        @Override
+        public QuestionMultiple createFromParcel(Parcel in) {
+            return new QuestionMultiple(in);
+        }
+
+        @Override
+        public QuestionMultiple[] newArray(int size) {
+            return new QuestionMultiple[size];
+        }
+    };
+
+    public String getUrl() {
+        return url;
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
     }
 
     public boolean isResponseCorrect(String responseChoose){
@@ -26,16 +59,26 @@ public class QuestionMultiple extends Question {
         this.reponses = reponses;
     }
 
-    @Override
-    public String getTitre() {
-        return super.getTitre();
-    }
-
     public String getResponse() {
         return response;
     }
 
     public String getIdImage() {
         return idImage;
+    }
+
+    @Override
+    public int describeContents() {
+        return 0;
+    }
+
+    @Override
+    public void writeToParcel(Parcel dest, int flags) {
+        dest.writeString(getNumQuestion());
+        dest.writeString(getTitre());
+        dest.writeString(idImage);
+        dest.writeString(url);
+        dest.writeStringList(reponses);
+        dest.writeString(response);
     }
 }

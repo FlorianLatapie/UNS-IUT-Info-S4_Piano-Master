f3bdb6b515253c84567dd2789acdc8c1ca427077
Florian LATAPIE <70631622+FlorianLatapie@users.noreply.github.com>
2021-03-19 20:45:40 +0100

    btn sms supprimé

--- app/src/main/java/com/example/pianomaster/LevelActivity.java
+++ app/src/main/java/com/example/pianomaster/LevelActivity.java
@@ -10,21 +10,11 @@ import android.widget.GridView;
 import android.widget.Toast;
 
 public class LevelActivity extends Activity {
-    private Context mContext = this;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_choix_niveau);
         GridView gridview = findViewById(R.id.gridView);
         gridview.setAdapter(new LevelAdapter(this));
-
-        Button b_sms_a_supprimer  = findViewById(R.id.b_sms_a_supprimer);
-        b_sms_a_supprimer.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(mContext, SMSActivity.class);
-                mContext.startActivity(intent);
-            }
-        });
     }
 }

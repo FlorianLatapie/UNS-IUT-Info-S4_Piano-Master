115565232f96f9c265dbe696c9d19822cc119b84
pierrebihannic <pierre.bihannic@etu.unice.fr>
2021-03-29 18:14:53 +0200

    Update ScoreActivity
    Ajout de onBackPressed

--- app/src/main/java/com/example/pianomaster/ScoreActivity.java
+++ app/src/main/java/com/example/pianomaster/ScoreActivity.java
@@ -1,5 +1,8 @@
 package com.example.pianomaster;
 
+import android.app.Activity;
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 
 import androidx.appcompat.app.AppCompatActivity;
@@ -20,5 +23,22 @@ public class ScoreActivity extends AppCompatActivity implements IntActivityClick
     public void onButtonClicked(String num) {
         getSupportFragmentManager().beginTransaction()
                 .add(R.id.frame_sms, new SMSActivity(num)).commit();
+    }
+
+    @Override
+    public void onBackPressed() {
+        SharedPreferences sp = getSharedPreferences("score", Activity.MODE_PRIVATE);
+        SharedPreferences sp2 = getSharedPreferences("niveau", Activity.MODE_PRIVATE);
+
+        SharedPreferences.Editor editor = sp.edit();
+        editor.putInt("getNiveau", 0);
+        editor.apply();
+
+        SharedPreferences.Editor editor2 = sp2.edit();
+        editor2.putInt("getScore", 0);
+        editor2.apply();
+        Intent intent = new Intent(this, LevelActivity.class);
+        intent.putExtra("numNiveau", 0);
+        startActivity(intent);
     }
 }
